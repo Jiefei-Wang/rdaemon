8ab0f5d8bd2cde343c8b59cbca8065185ee74568
Jiefei Wang <szwjf08@gmail.com>
2021-08-24 11:34:06 +0800

    support Linux in detachConsole

--- src/daemon.cpp
+++ src/daemon.cpp
@@ -1,7 +1,13 @@
+#include <Rinternals.h>
+#include <string>
 
 #ifdef _WIN32
 #include <windows.h>
 #else
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/prctl.h>
 #endif
 
 // [[Rcpp::export]]
@@ -10,6 +16,21 @@ void detachConsole()
 #ifdef _WIN32
     FreeConsole();
 #else
-    //int sid = setsid();
+     if ( getppid() == 1 ) {
+         Rf_error("Cannot detach group leader!");
+     }
+    int sid = setsid();  
+    if (sid < 0)    
+    {  
+        Rf_error("Fail to detach! Error: %s", strerror(errno));
+    }  
+#endif
+}
+
+// [[Rcpp::export]]
+void setProcessName(std::string name){
+#ifdef _WIN32
+#else
+    prctl(PR_SET_NAME, (unsigned long)"test", 0, 0, 0);
 #endif
 }
