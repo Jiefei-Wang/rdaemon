08a7ac4310dedf2f1a10195eeff2f2b30cbac738
Jiefei Wang <szwjf08@gmail.com>
2021-09-07 13:05:34 +0800

    update isProcessAlive function
    Merge branch 'master' of github.com:Jiefei-Wang/rdaemon
    
    # Conflicts:
    #	src/detachConsole.cpp

--- R/utiles-export.R
+++ R/utiles-export.R
@@ -38,16 +38,5 @@ interruptProcess <- function(pid){
 #' @rdname daemon-utils
 #' @export
 isProcessAlive <- function(pid){
-    tryCatch(
-        if(Sys.info()[['sysname']]=="Windows"){
-            out = system2("wmic",
-                          paste0('process where "ProcessID = ',pid, '" get processid'),
-                          stdout = TRUE)
-            any(grepl(pid, out, fixed = TRUE))
-        }else{
-            system2("ps", c("-p", pid), stdout = NULL, stderr = NULL) == 0L
-        },
-        warning = function(e) TRUE,
-        error = function(e) TRUE
-    )
+    isProcessRunning(pid)
 }
--- src/isProcessRunning.cpp
+++ src/isProcessRunning.cpp
@@ -15,8 +15,14 @@ bool isProcessRunning(long long unsigned pid)
     bool exist = false;
     #ifdef _WIN32
     HANDLE process = OpenProcess(SYNCHRONIZE, FALSE, pid);
-    if(process == NULL)
-        return false;
+    if(process == NULL){
+        DWORD errorMessageID = ::GetLastError();
+        if(errorMessageID == ERROR_ACCESS_DENIED){
+            return true;
+        }else{
+            return false;
+        }
+    }
     DWORD ret = WaitForSingleObject(process, 0);
     CloseHandle(process);
     exist = (ret == WAIT_TIMEOUT);
